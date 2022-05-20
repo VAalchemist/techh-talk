8f98d294096ff2917c57aae43e81f391fbae6c40
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-05-20 15:39:12 -0700

    PUT method set for posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -88,7 +88,30 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.put('/:')
+router.put('/:id', withAuth, (req, res) => {
+  Post.update(
+    {
+      title: req.body.title
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with that id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 
 
