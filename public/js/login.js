5fce7a746043d594a1ffbc6b8a849a557a0df67c
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-04 23:47:28 -0700

    signup logic added

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 async function loginFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
@@ -23,3 +23,35 @@ async function loginFormHandler(event) {
     }
   }
 }
+
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        email,
+        password
+      }),
+
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    }
+    else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
