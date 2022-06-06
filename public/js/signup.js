c963b0032d06f23cdafc8e8c5a55f2933ce8e64f
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-06 14:37:08 -0700

    signup view and logic set but not working

---
+++ public/js/signup.js
@@ -0,0 +1,23 @@
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+  
+  const usernameEl = document.querySelector("#usrnm-signup");
+  const passwordEl = document.querySelector("#pwd-signup");
+
+  fetch("/api/user", {
+    method: "post",
+    body: JSON.stringify({
+      username: usernameEl.value,
+      password: passwordEl.value
+    }),
+    headers: { "Content-Type": "application/json" }
+  })
+  .then(function() {
+      document.location.replace("/dashboard");
+  })
+  .catch(err => console.log(err));
+} 
+
+document
+  .querySelector("#signup-form")
+  .addEventListener("submit", signupFormHandler);
