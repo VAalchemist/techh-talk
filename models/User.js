805b05f7d144bf0293391e7933550e331fd531b0
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-07 01:50:56 -0700

    user model/controller set

--- models/User.js
+++ models/User.js
@@ -22,40 +22,44 @@ User.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
-
-  },
-
-  username: {
+    },
+    username: {
       type: DataTypes.STRING,
       allowNull: false
-
-
-  },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
 
   password: {
       type: DataTypes.STRING,
       allowNull: false,
     validate: {
       len: [6]
-}
+    }
   }
 },
-  
-  {
+{
   // TABLE CONFIGURATION OPTIONS GO HERE
+
     hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
+        return newUserData;
       },
-      
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
+        return updatedUserData;
       }
     },
-    
-  // pass in our imported sequelize connection (the direct connection to our database)
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
@@ -65,7 +69,6 @@ User.init(
     underscored: true,
     // make it so our model name stays lowercase in the database
     modelName: 'user'
-
-});
+  });
 
 module.exports = User;
