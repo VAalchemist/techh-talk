9d76ce57af6f910052f1b0c4987de8bbc828c1ca
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-07 01:52:13 -0700

    comment model/controller set

--- models/Comment.js
+++ models/Comment.js
@@ -1,23 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class Comment extends Model {}
 
-class Comments extends Model {}
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+  }
+);
 
-//create fields(columns) for Comment model
-// Comments.init({
-//   body: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   }  
-// },
-//   {
-//     sequelize,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'comment'
-// });
-
-
-
-module.exports = Comments;
+module.exports = Comment;
