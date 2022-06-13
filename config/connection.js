043bb12b9c2a0254753651a7a6873452479e9d8f
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-06-12 20:25:46 -0700

    tried to fix connection file for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-// creating connection to db
-
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
-
-// sequelize.authenticate().then(() => {
-//   console.log("connection successful!");
-// }).catch((err) => {
-//   console.log(":( NO success connecting to db!");
-// });
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
